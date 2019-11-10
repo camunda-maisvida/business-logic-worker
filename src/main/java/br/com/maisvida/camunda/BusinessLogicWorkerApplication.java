1bb88eb9b024943f5c8d8ff0cd120a3ef82cfd01
Rodolfo Cruz <rodolfocruz.ti@gmail.com>
2019-11-10 12:13:50 -0200

    Enabling eureka client

--- src/main/java/br/com/maisvida/camunda/BusinessLogicWorkerApplication.java
+++ src/main/java/br/com/maisvida/camunda/BusinessLogicWorkerApplication.java
@@ -2,10 +2,12 @@ package br.com.maisvida.camunda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
 public class BusinessLogicWorkerApplication {
 
 	public static void main(String[] args) {
